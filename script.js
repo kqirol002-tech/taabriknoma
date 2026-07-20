f671c89330b09501fe16c192ba1fccdbbd80f69b
raupovv <kqirol002@gmail.com>
2026-07-21 00:22:47 +0500

    function bg added

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let dizayn = document.querySelector(".flex-btn-1");
 let none = document.querySelector(".none");
 let div = document.querySelector(".project");
 let bg = document.querySelector(".bg");
+let bg_modal = document.querySelector(".bg-modal");
 
 tugma.addEventListener("click", (e) => {
   e.preventDefault();
@@ -42,6 +43,10 @@ pink.addEventListener("click", (e1) => {
     "url('https://template.canva.com/EAGS_pIbYkY/1/0/1131w-jL4tQYA06a8.jpg')";
   bg.style.backgroundSize = "cover";
   bg.style.backgroundPosition = "center";
+  bg_modal.style.backgroundImage =
+    "url('https://template.canva.com/EAGS_pIbYkY/1/0/1131w-jL4tQYA06a8.jpg')";
+  bg_modal.style.backgroundSize = "cover";
+  bg_modal.style.backgroundPosition = "center";
 });
 blue.addEventListener("click", (e2) => {
   e2.preventDefault();
@@ -50,6 +55,10 @@ blue.addEventListener("click", (e2) => {
     "url('https://i.pinimg.com/736x/db/d6/84/dbd684dfc4f653093cf51599c87dbef4.jpg')";
   bg.style.backgroundSize = "cover";
   bg.style.backgroundPosition = "center";
+  bg_modal.style.backgroundImage =
+    "url('https://i.pinimg.com/736x/db/d6/84/dbd684dfc4f653093cf51599c87dbef4.jpg')";
+  bg_modal.style.backgroundSize = "cover";
+  bg_modal.style.backgroundPosition = "center";
 });
 orange.addEventListener("click", (e3) => {
   e3.preventDefault();
@@ -58,6 +67,10 @@ orange.addEventListener("click", (e3) => {
     "url('https://img.magnific.com/premium-photo/festive-birthday-cupcake-with-balloons-confetti-orange-background_1298956-7555.jpg?semt=ais_hybrid&w=740&q=80')";
   bg.style.backgroundSize = "cover";
   bg.style.backgroundPosition = "center";
+  bg_modal.style.backgroundImage =
+    "url('https://img.magnific.com/premium-photo/festive-birthday-cupcake-with-balloons-confetti-orange-background_1298956-7555.jpg?semt=ais_hybrid&w=740&q=80')";
+  bg_modal.style.backgroundSize = "cover";
+  bg_modal.style.backgroundPosition = "center";
 });
 green.addEventListener("click", (e4) => {
   e4.preventDefault();
@@ -66,6 +79,10 @@ green.addEventListener("click", (e4) => {
     "url('https://img.magnific.com/premium-vector/birthday-photo-frame-event-party-bitrhday-celebration-gold-glitter-green-background_1023797-1561.jpg?semt=ais_hybrid&w=740&q=80')";
   bg.style.backgroundSize = "cover";
   bg.style.backgroundPosition = "center";
+   bg_modal.style.backgroundImage =
+    "url('https://img.magnific.com/premium-vector/birthday-photo-frame-event-party-bitrhday-celebration-gold-glitter-green-background_1023797-1561.jpg?semt=ais_hybrid&w=740&q=80')";
+  bg_modal.style.backgroundSize = "cover";
+  bg_modal.style.backgroundPosition = "center";
 });
 bluewiolet.addEventListener("click", (e5) => {
   e5.preventDefault();
@@ -74,6 +91,10 @@ bluewiolet.addEventListener("click", (e5) => {
     "url('https://static.vecteezy.com/system/resources/previews/060/298/187/non_2x/purple-background-with-golden-happy-birthday-greeting-and-many-colorful-balloons-flying-upright-vector.jpg')";
   bg.style.backgroundSize = "cover";
   bg.style.backgroundPosition = "center";
+  bg_modal.style.backgroundImage =
+    "url('https://static.vecteezy.com/system/resources/previews/060/298/187/non_2x/purple-background-with-golden-happy-birthday-greeting-and-many-colorful-balloons-flying-upright-vector.jpg')";
+  bg_modal.style.backgroundSize = "cover";
+  bg_modal.style.backgroundPosition = "center";
 });
 imgInp.onchange = (evt) => {
   const [file] = imgInp.files;
